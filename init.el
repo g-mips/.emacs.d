83f3a2bba5b445936c2de236699c8ca4d048fe31
g-mips <none>
2018-05-15 00:46:31 -0600

    Needed time on bar and highlighting. Removed startup screen as well. Allowed theme to be grabbed from xresources-theme

--- init.el
+++ init.el
@@ -8,6 +8,16 @@
   (package-refresh-contents)
   )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-time-mode t)
+ '(global-hl-line-mode 1)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (xresources-theme))))
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
