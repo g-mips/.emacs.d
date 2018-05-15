b4f2db44b48650cac7d55b43dd1cdc681dab2ce2
g-mips <none>
2018-05-15 00:37:55 -0600

    Added package handling

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;; Package Handling ;;;;;;;;;;;;;;;;;;;;;;
+(when (>= emacs-major-version 24)
+  (require 'package)
+  ;; ELPA is automatically added.
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://stable.melpa.org/packages/") t)
+  (package-initialize)
+  (package-refresh-contents)
+  )
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
