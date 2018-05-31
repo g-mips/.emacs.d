ef7cbc4f14f6d1803b0de67dd117bad252f5da16
g-mips <none>
2018-05-30 23:26:29 -0600

    Remoed smart-mode-line-powerline-theme. Removed time and battery display on mode line.

--- mode-line.el
+++ mode-line.el
@@ -1,15 +1,11 @@
 ; ---- Install Packages ---- ;
 (if (not (package-installed-p 'smart-mode-line))
     (package-install 'smart-mode-line))
-(if (not (package-installed-p 'smart-mode-line-powerline-theme))
-    (package-install 'smart-mode-line-powerline-theme))
 
-(require 'smart-mode-line-powerline-theme)
-
-(setq powerline-default-separator 'arrow)
-(setq display-time-day-and-date 1)
-
+(setq sml/theme 'respectful)
 (sml/setup)
 (sml/toggle-shorten-directory)
 
-(display-battery-mode 1)
+(display-time-mode -1)
+(display-battery-mode -1)
+
